51dca87faace1bdf5e3fb6ad1a3894899dd8dfab
Quiatus <Quiatus@gmail.com>
2024-04-29 23:15:38 +0200

    bug

--- script/modules/dom/tavern.js
+++ script/modules/dom/tavern.js
@@ -4,7 +4,7 @@ export const displayTavern = () => {
     <h1>Tavern</h1>
 
     <p class="text-i mb">You enter the tavern and give a nod to the barkeep. He nods back and starts pouring you an ale into a mug. You then look around the place and see a few regulars, some talking quietly, some playing a game of dice and some
-    already slumped across the table. You also see a few unfamiliar faces. </p>
+    already slumped across the table. You also see a few unfamiliar faces.</p>
 
     <div id="tavern">  
         
